7b0fcdc744db502e9da24eadcd6ee165a6d2b0d3
joeycrz <109363676+joeycrz@users.noreply.github.com>
2023-09-11 00:57:08 -0500

    refactored homepage

--- my_app/src/pages/Home.tsx
+++ my_app/src/pages/Home.tsx
@@ -1,27 +1,69 @@
-import { Container, Col, Row, Image } from 'react-bootstrap';
+import { Container, Col, Row, Image, Button, Modal, ModalProps } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/src/modal'
 import './pages.css';
 import me from '../images/me.png';
+import { useState } from 'react'
+import resume1 from '../images/resume_NoMargin - Copy-1.png';
+
+import { SocialIcon } from 'react-social-icons';
+import React from 'react';
+
 export function Home() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
         <Container fluid>
             <Row className='mb-3'>
-                <Container className='cc'>
-                    <div className='profilePic'></div>
+                <Container className='cc1'>
+                    <img src={me} className='profilePic'/>
                 </Container>
 
             </Row>
-            <Row md='auto'>
+            <Row >
                 <Container className='cc'>
                     <div className='description example'>
                         <h3 className='descriptionH'>Welcome to my portfolio!</h3>
-                        <p>To whoever is reading this, welcome! My name is Jose Cruz, but my friends call me Joey, I
+                        <p>Hello! My name is Jose Cruz, but my friends call me Joey, I
                             am currently a senior studying computer science at the University of Texas
                             Rio Grande Valley. Feel free to explore the page and check out any information provided.
+                            Below you will find my resume and links to my socials!
                         </p>
                     </div>
                 </Container>
             </Row>
             <Row className='mb-3'>
+                <Container className='resumeButtonCont'>
+                    <div className='cc'>
+                        <button data-bs-toggle="modal" data-bs-target="#imageExample">Resume</button>
+                    </div>
+
+
+                    <div className='modal fade' id='imageExample' tabIndex={-1} aria-hidden='true'>
+                        <div className='modal-dialog'>
+                            <div className='modal-content resumeCont'>
+                                <div className='modal-body '>
+                                    <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                                    <img src={resume1} className='d-block resume ' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </Row>
+            <Row >
+                <Container className='cc'>
+                    <SocialIcon url="https://www.linkedin.com/in/jose-a-cruziii/" className='social' />
+                    <SocialIcon url="https://github.com/joeycrz" className='social' />
+                    <SocialIcon url="https://open.spotify.com/user/deathissolution" className='social' />
+                    <SocialIcon url="https://www.instagram.com/joey.cz/" />
+                </Container>
+
+            </Row>
+
+            {/* <Row className='mb-3'>
                 <Col xs lg={6}>
                     <Container className='sectionBack shadow-lg'>
                         <div className='example2'>
@@ -73,7 +115,8 @@ export function Home() {
                         you should be able to obtain a better understanding of what I have been able to work with. This portfolio webpage being one of them!</p>
                     </div>
                 </Container>
-            </Row>
+            </Row> */}
         </Container>
+
     )
 }
--- my_app/src/pages/Socials.tsx
+++ my_app/src/pages/Socials.tsx
@@ -1,8 +1,14 @@
-export function Socials(){
-    return(
-        <>
-            <p>Socials</p>
-        
-        </>
+import { Container, Row, Col } from "react-bootstrap";
+import { SocialIcon } from 'react-social-icons';
+
+export function Socials() {
+    return (
+        <Container className="cc">
+            <Row className="mr-5">
+                    <SocialIcon url="https://twitter.com" />
+            </Row>
+         
+
+        </Container>
     )
 }
